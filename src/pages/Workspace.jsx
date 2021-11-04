cb41011369be3df75c4d66d73764ede26b443f0d
ahamad2 <abdullah.hamad@huskers.unl.edu>
2021-11-04 14:07:59 -0500

    Added some of the basic elements to the Workspace.

--- src/pages/Workspace.jsx
+++ src/pages/Workspace.jsx
@@ -1,8 +1,26 @@
 import React from 'react';
+import './Workspace.css';
+import small_RNA_SVG from '../graphics/small_RNA_SVG.svg';
+import scaffold_rna_button from '../graphics/scaffold_button.svg';
+
 
 function Workspace() {
     return (
-       <div></div>
+       <div>
+           <header className='rna-title'>RNAMake</header>
+           <h2 className='rna-subtitle'>3D Design Toolkit</h2>
+
+           <section>
+               <aside>
+                   <img src={small_RNA_SVG} float='right'></img>
+               </aside>
+               <img src={scaffold_rna_button} float='right'></img>
+           </section>
+
+           <section color='#E4E9F1' float='right'>
+               <text>Recent Experiments</text>
+           </section>
+       </div>
     );
 }
 
