53f5c95bcc8fdfae046721d48073e826ddb963a9
kkwertz11 <kkwertz11@huskers.unl.edu>
2021-11-04 13:46:26 -0500

    finished basic parameters page for job submission

--- src/components/JobName.jsx
+++ src/components/JobName.jsx
@@ -2,20 +2,15 @@ import * as React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-//import Paper from '@mui/material/Paper';
-//import Divider from '@mui/material/Divider';
-//import MenuList from '@mui/material/MenuList';
-//import MenuItem from '@mui/material/MenuItem';
-//import ListItemIcon from '@mui/material/ListItemIcon';
-//import ListItemText from '@mui/material/ListItemText';
-//import Check from '@mui/icons-material/Check';
+import BasicMenu from './BasicMenu';
+
 
 
 export default function JobName() {
   return (
     <React.Fragment>
       <Typography variant="h6" gutterBottom>
-        Name
+        Project Name/Description
       </Typography>
       <Grid container spacing={3}>
         <Grid item xs={12} sm={6}>
@@ -38,49 +33,8 @@ export default function JobName() {
             variant="standard"
           />
         </Grid>
-        <Grid item xs={12}>
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="city"
-            name="city"
-            label="City"
-            fullWidth
-            autoComplete="shipping address-level2"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            id="state"
-            name="state"
-            label="State/Province/Region"
-            fullWidth
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="zip"
-            name="zip"
-            label="Zip / Postal code"
-            fullWidth
-            autoComplete="shipping postal-code"
-            variant="standard"
-          />
-        </Grid>
-        <Grid item xs={12} sm={6}>
-          <TextField
-            required
-            id="country"
-            name="country"
-            label="Country"
-            fullWidth
-            autoComplete="shipping country"
-            variant="standard"
-          />
+        <Grid item xs={12 }>
+        <BasicMenu />
         </Grid>
       </Grid>
     </React.Fragment>
