5a58bd02a20d69557faf7512e95c2e0a3c896a9e
tmcelmury <trentamcelmury@gmail.com>
2021-11-08 11:33:04 -0600

    About page is ready for release 2

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ ReactDOM.render(
 );
 export { default as Navigation } from './components/Navigation';
 export { default as Home } from './pages/Home';
-export { default as About } from './pages/About';
+export { default as About } from './pages/About/About';
 export { default as Contact } from './pages/Contact';
 export { default as Documentation } from './pages/Documentation';
 export { default as Workspace } from './pages/Workspace';
