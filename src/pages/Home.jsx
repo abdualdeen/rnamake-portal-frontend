47f8a0fda55cf90367d28e8e81f840a29cd5809e
ahamad2 <abdullah.hamad@huskers.unl.edu>
2021-11-23 12:21:57 -0600

    changed size and position of butons.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,7 @@
 ﻿import React from 'react';
 import "./Home.css";
 import { Button } from '../components/Button';
-import homeswoop from "../graphics/HomeSwoop.svg"
+import homeSwoop from "../graphics/HomeSwoop.svg"
 
 function Home() {
 
@@ -15,16 +15,18 @@ function Home() {
                with helix flanking both sides allows users of RNAMake to build large segments of RNA with a high success rate of 
                forming the predicted structure in vitro.<br /></div>
             <div className="main-text" align-text="center"><br />Create an account or log in to access the application<br></br></div>
-            <Button display="inline" right="-20%" top="750px" onClick={() => {}}
-                        type="button"
-                        buttonStyle="btn--primary--solid"
-                        buttonSize="btn--medium">Log In</Button>
-            <Button display="inline" right="-15%" top="750px" onClick={() => {}}
-                        type="button"
-                        buttonStyle="btn--primary--solid"
-                        buttonSize="btn--medium">Register</Button>
+            <div className="home-btns">
+              <Button onClick={() => {}}
+                          type="button"
+                          buttonStyle="btn--primary--solid"
+                          buttonSize="btn--large">Login</Button>
+              <Button className="register-btn" onClick={() => {}}
+                          type="button"
+                          buttonStyle="btn--primary--solid"
+                          buttonSize="btn--large">Register</Button>
+            </div>
           </div>
-          <img className="swoop" src={homeswoop} />
+          <img className="swoop" src={homeSwoop}/>
       </div>
     );
 }
